0b004a9d52ba6b4c85c02e336bc95b52d0adef07
CapBlack <139591133+CapBlack414@users.noreply.github.com>
2023-11-30 02:11:19 +0200

    Update smile.bat

--- smile.bat
+++ smile.bat
@@ -1,4 +1,4 @@
-cd C:\Users\pc\Desktop
+cd %USERPROFILE%/Desktop
 Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/CapBlack414/Hash0x1/main/smile.jpg' -outfile mowafy.jpg"
 copy mowafy.jpg  mowafy-.jpg
 copy mowafy-.jpg  mowafy--.jpg
